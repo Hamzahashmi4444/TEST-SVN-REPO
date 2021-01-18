ad98ab7a6c5f8b2e8f0b560e1eb407547ade2824
hamza <hamza@gmail.com>
2021-01-18 05:56:24 +0000

    Commiting Nayaa repo
    git-svn-id: https://desktop-hqjj4hj/svn/Nayaa@2 38bd177b-df91-7445-a56c-6c2d442fc1e9

---
+++ trunk/eggs/Naaya/trunk/Products/NaayaCore/NotificationTool/constants.py
@@ -0,0 +1,8 @@
+LOG_TYPES = {
+    'approved': 'Object approved',
+    'created': 'Object created',
+    'modified': 'Object modified',
+    'commented': 'Comment added on object',
+    'bulk_import': 'Notification: bulk import (zip/csv)',
+}
+DISABLED_EMAIL = u'disabled@eionet.europa.eu'
